a7999faf10abff7fecf5b86d03ed37fc6053c10d
rehmax <obaidurrehman164@gmail.com>
2025-02-20 22:54:10 -0800

    Adds a helper method 'add_task' to 'taskUtils' class to add tasks to 'task_manager'.

--- src/utils/include/taskUtils.hpp
+++ src/utils/include/taskUtils.hpp
@@ -1,13 +1,17 @@
 #ifndef TASK_UTILS
 #define TASK_UTILS
 
+#include <asyncTask.h>
+#include <asyncTaskManager.h>
+#include <memory>
+
+// Helper function to create an inline task
 template<class Callable>
-AsyncTask *make_task(Callable callable, const std::string &name, int sort = 0, int priority = 0) {
+AsyncTask* make_task(Callable callable, const std::string& name, int sort = 0, int priority = 0) {
     class InlineTask final : public AsyncTask {
     public:
-        InlineTask(Callable callable, const std::string &name, int sort, int priority) :
-                AsyncTask(name),
-                _callable(std::move(callable)) {
+        InlineTask(Callable callable, const std::string& name, int sort, int priority) :
+                AsyncTask(name), _callable(std::move(callable)) {
             _sort = sort;
             _priority = priority;
         }
@@ -22,6 +26,12 @@ AsyncTask *make_task(Callable callable, const std::string &name, int sort = 0, i
         Callable _callable;
     };
     return new InlineTask(std::move(callable), name, sort, priority);
+}
+
+// Utility Function to create and add a task in one step
+template<class Callable>
+void add_task(Callable callable, const std::string& name, int sort = 0, int priority = 0) {
+    AsyncTaskManager::get_global_ptr()->add(make_task(std::move(callable), name, sort, priority));
 }
 
 #endif // TASK_UTILS
